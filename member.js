7bda67985eec60ee9231bedb247633999a69738f
nuskudar <nuskudar@outlook.com>
2023-12-23 18:57:23 +0000

    Update Member with ctrl + enter(Completion Panel)

--- member.js
+++ member.js
@@ -1,3 +1,18 @@
-function skillsMember() {
-    console.log('I am a member')
+function skillsMember(){
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var memberIcon = document.getElementById("member-icon");
+  var skillsIcon = document.getElementById("skills-icon");
+  var projectsIcon = document.getElementById("projects-icon");
+  var contactIcon = document.getElementById("contact-icon");
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+  memberIcon.style.color = "#b3b3b3";
+  skillsIcon.style.color = "#fff";
+  projectsIcon.style.color = "#fff";
+  contactIcon.style.color = "#fff";
 }
